327c096ce6a0233a4a50f495cbbc9cada0fc3a9e
WodaMineralna <raknieborak65@gmail.com>
2025-04-17 13:38:21 +0200

    data fetching custom hook creation + .eslintrc config

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+export function useFetch(fetchFn, initialValue, customErrorMsg) {
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || customErrorMsg });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn, customErrorMsg]);
+
+  return { fetchedData, isFetching, error }
+}
